b8d40f39e533ef235701be29d97c9c544fa7ea32
Antoine DALLE RIVE <antoine.dalle@soprasteria.com>
2017-04-24 22:04:31 +0200

    poc task parallel foreach

---
+++ CSharp.Demo/FilesGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CSharp.Demo
+{
+    public class FilesGenerator
+    {
+        public static List<string> CopyFile(string filePath, int nb, string outputDir)
+        {
+            var dirInfo = new DirectoryInfo(outputDir);
+            List<string> files = new List<string>();
+
+            dirInfo.GetFiles().ToList().ForEach(f => f.Delete());
+
+            for (int indexFile = 0; indexFile < nb; indexFile++)
+            {
+                string outPutFileName = Path.Combine(outputDir, string.Format("{0}.{1}{2}", Path.GetFileNameWithoutExtension(filePath), indexFile, Path.GetExtension(filePath)));
+                File.Copy(filePath, outPutFileName, true);
+            }
+
+            return Directory.GetFiles(outputDir).ToList();
+        }
+    }
+}
--- CSharp.Demo/Program.cs
+++ CSharp.Demo/Program.cs
@@ -1,15 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CSharp.Demo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Start");
+            Stopwatch watch = Stopwatch.StartNew();
+            try
+            {
+                string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+                int nbCopy = 100;
+                string fileName = "document.pdf";
+                string inputDir = Path.Combine(currentDir, "workingdirectory");
+                Directory.CreateDirectory(inputDir);
+                List<string> files;
+
+                files = FilesGenerator.CopyFile(fileName, nbCopy, inputDir);
+
+                #region classic foreach
+
+                Console.WriteLine("Starting classic foreach loop");
+
+                watch.Restart();
+
+                foreach (var file in files)
+                {
+                    File.Encrypt(file);
+                }
+
+                watch.Stop();
+
+                Console.WriteLine("Encrypt foreach files : {0}s", watch.Elapsed.TotalSeconds);
+
+                #endregion classic foreach
+
+                files = FilesGenerator.CopyFile(fileName, nbCopy, inputDir);
+
+                #region parallel loop
+
+                Console.WriteLine("Starting parallel loop");
+                watch.Restart();
+
+                Parallel.ForEach(files, f => File.Encrypt(f));
+
+                watch.Stop();
+                Console.WriteLine("Encrypt Parallel files : {0}s", watch.Elapsed.TotalSeconds);
+
+                #endregion parallel loop
+
+                files = FilesGenerator.CopyFile(fileName, nbCopy, inputDir);
+
+                #region tasks loop
+
+                Console.WriteLine("Starting tasks loop");
+                watch.Restart();
+
+                List<Task> tasks = new List<Task>();
+                foreach (string file in files)
+                {
+                    tasks.Add(Task.Run(() => File.Encrypt(file)));
+                }
+
+                Console.WriteLine("Extra work waiting encrypt ...");
+
+                Task.WaitAll(tasks.ToArray());
+
+                watch.Stop();
+                Console.WriteLine("Encrypt async files : {0}s", watch.Elapsed.TotalSeconds);
+
+                #endregion tasks loop
+            }
+            catch (System.Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex);
+                Console.ResetColor();
+            }
+            Console.WriteLine("End");
+            Console.ReadLine();
         }
     }
 }
